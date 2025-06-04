4267918304f7197ec5f77e5ac44546c2e5ed00b0
Ken Muyesu <kmuyesu@gmail.com>
2025-06-04 05:19:56 +0300

    chore: add multiplication functionality

--- cmd/zero.go
+++ cmd/zero.go
@@ -36,3 +36,23 @@ func Subtract(from, subtract string) (result string) {
 
 	return fmt.Sprintf("%f", num1-num2)
 }
+
+func Multiply(first, second string, shouldRoundUp bool) (result string) {
+	num1, err := strconv.ParseFloat(first, 64)
+	if err != nil {
+		fmt.Println("Error: First value is not a decimal")
+		return
+	}
+
+	num2, err := strconv.ParseFloat(second, 64)
+	if err != nil {
+		fmt.Println("Error: Second value is not a decimal")
+		return
+	}
+
+	if shouldRoundUp {
+		return fmt.Sprintf("%.2f", num1*num2)
+	}
+
+	return fmt.Sprintf("%f", num1*num2)
+}
